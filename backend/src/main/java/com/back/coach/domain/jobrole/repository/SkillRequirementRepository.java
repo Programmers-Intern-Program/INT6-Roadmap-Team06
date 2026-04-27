c369214cdf0367d98f42cac30d5beb0aa03bb051
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2026-04-27 17:33:04 +0900

    feat(domain): 직무 기술 요구사항 저장 엔티티 추가

---
+++ backend/src/main/java/com/back/coach/domain/jobrole/repository/SkillRequirementRepository.java
@@ -0,0 +1,7 @@
+package com.back.coach.domain.jobrole.repository;
+
+import com.back.coach.domain.jobrole.entity.SkillRequirement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillRequirementRepository extends JpaRepository<SkillRequirement, Long> {
+}
